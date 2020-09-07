c71c4bd9e231b9cccfe0b7d96ce18a546ba8a900
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2020-09-07 17:45:41 -0400

    Remove Wnck

--- imagechooser.py
+++ imagechooser.py
@@ -21,7 +21,6 @@ import os
 from gi.repository import GObject
 from gi.repository import Gtk
 from gi.repository import Gdk
-from gi.repository import Wnck
 
 from sugar3.graphics import style
 from sugar3.graphics.toolbutton import ToolButton
@@ -76,11 +75,6 @@ class ImageFileChooser(Gtk.Window):
 
         if parent is None:
             logging.warning('ObjectChooser: No parent window specified')
-        else:
-            self.connect('realize', self.__realize_cb, parent)
-
-            screen = Wnck.Screen.get_default()
-            screen.connect('window-closed', self.__window_closed_cb, parent)
 
         self._vbox = Gtk.VBox()
         self.add(self._vbox)
